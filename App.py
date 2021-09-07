28c68d66f00ab9df24941bc00b33a00b7041f4bf
Henry-Grace <92324@farnborough.ac.uk>
2021-09-07 14:33:31 +0100

    -

---
+++ App.py
@@ -0,0 +1,16 @@
+print("hello world")
+
+print("   /!")
+print("  / !")
+print(" /  !")
+print("/___!")
+
+
+CharName = "Jeff"
+
+CharAge = "65"
+
+print("There once was a man named " + CharName)
+print("He was " + CharAge + " years old")
+print("He really likes the name " + CharName)
+print("but didn't like being " + CharAge)
